aa57f9d980c33b90ee2bea7db2fd3235ae828204
Yosendra <yosendra.evri@gmail.com>
2023-11-04 21:47:02 +0800

    Operators - 1. Arithmetic Operators

---
+++ 1. Fundamentals/2. Operators/1-Arithmetic-Operators/index.js
@@ -0,0 +1,24 @@
+let x = 10;
+let y = 3;
+
+// x and y are operand. x + y is an expression. 
+// Expression always produce a value
+console.log(x + y);
+
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);         // Remainder of division
+console.log(x ** y);        // Exponentiation
+
+let a = 7; 
+let b = 3;
+// Post-Increment Operator
+console.log(a++);   // Output: 7
+// Pre-Increment Operator
+console.log(++a);   // Output: 9
+
+// Post-Decrement Operator
+console.log(b--);   // Output: 3
+// Pre-Decrement Operator
+console.log(--b);   // Output: 1
