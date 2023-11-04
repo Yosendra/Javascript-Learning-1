3d2350ef399313002a1a5045907ff5baaf20373c
Yosendra <yosendra.evri@gmail.com>
2023-11-04 20:18:45 +0800

    Basic - 6. Array

---
+++ 1. Fundamentals/1. Basics/6-Arrays/index.js
@@ -0,0 +1,10 @@
+let selectedColor = ['red', 'blue'];    // Define an array
+
+selectedColor[2] = 1;
+
+// The size of array is dynamic.
+console.log(selectedColor);
+
+// Array is an object in Javascript, so you can use dot notation on an array
+console.log(typeof selectedColor);
+console.log(selectedColor.length);      // Output: 3
