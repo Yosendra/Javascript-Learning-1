0a90dba5021205d3ec31a0da74f6d89416f7d2c0
Yosendra <yosendra.evri@gmail.com>
2023-11-04 19:13:44 +0800

    Basic - 1. Variables

---
+++ 1. Fundamentals/1. Basics/1-Variables/index.js
@@ -0,0 +1,10 @@
+// Variable is used for storing data temporarily in the computer memory
+// Use let or const keyword only for defining variable (var is deprecated)
+
+let name = 'Yosendra';
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (ex: 1name)
+// Cannot contain a space or hyphen (-)
