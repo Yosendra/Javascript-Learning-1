2fa6b59276517483e6ae45f3d1730753a5b6556d
Yosendra <yosendra.evri@gmail.com>
2023-11-04 19:32:47 +0800

    Basic - 3. Primitive Types

---
+++ 1. Fundamentals/1. Basics/3-Primitve-Types/index.js
@@ -0,0 +1,10 @@
+// There are two categories of type
+// 1. Primitive (Value Type)
+//  -> String, Number, Boolean, Undefined, null
+// 2. Reference Type
+
+let name = "Yosendra";      // String literal
+let number = 30;            // Number literal
+let isApproved = true;      // Boolean literal
+let firstName = undefined;  // Undefined
+let selectedColor = null;   // Null
