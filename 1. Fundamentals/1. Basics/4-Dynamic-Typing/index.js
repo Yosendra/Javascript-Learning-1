5c65f4b2db4bba7672f8fcb0428168ae6a619f91
Yosendra <yosendra.evri@gmail.com>
2023-11-04 19:50:37 +0800

    Basic - 4. Dynamic Typing

---
+++ 1. Fundamentals/1. Basics/4-Dynamic-Typing/index.js
@@ -0,0 +1,18 @@
+// Javascript is Dynamic-Type language
+// Two Kinds of Programming Language
+//  -> Static typed. 
+//      Once defined, cannot be changed to another type.
+//      string name = "Yosendra"
+//  -> Dynamic typed
+//      Once defined, still can be changed to another type.
+//      let name = "Yosendra"
+
+let name = "Yosendra";      // String literal
+let number = 30;            // Number literal
+let isApproved = true;      // Boolean literal
+let firstName = undefined;  // Undefined
+let selectedColor = null;   // Null
+
+console.log(typeof name);   // Output: string
+name = 1;                   // Value type change to number
+console.log(typeof name);   // Output: number
