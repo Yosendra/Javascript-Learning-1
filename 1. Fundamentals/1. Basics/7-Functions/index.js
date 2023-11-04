b592c16e41c715ac5d4974b39f8b8c0486742a8f
Yosendra <yosendra.evri@gmail.com>
2023-11-04 20:28:39 +0800

    Basic - 7. Function

---
+++ 1. Fundamentals/1. Basics/7-Functions/index.js
@@ -0,0 +1,12 @@
+// Function basically a set of statement that performs a task or calculates a value
+
+// Function definition
+// 'name' is called parameter
+function greet(name){
+    console.log("Hello " + name)
+}   // When define a function there is no need to attact semicolon at the end
+
+// Calling the function
+// 'Yosendra' is called argument
+greet("Yosendra");  // Output: Hello Yosendra
+greet("John");      // Output: Hello John
