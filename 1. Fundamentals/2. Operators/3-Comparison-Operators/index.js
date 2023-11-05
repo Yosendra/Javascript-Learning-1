bc0a99d9db4484ebffb279ee112c26daf292e737
Yosendra <yosendra.evri@gmail.com>
2023-11-05 10:07:40 +0800

    Operators - 3. Comparison Operators

---
+++ 1. Fundamentals/2. Operators/3-Comparison-Operators/index.js
@@ -0,0 +1,13 @@
+let x = 1;
+
+// The result of an expression that involves comparisons operator is boolean
+
+// Relational
+console.log(x > 0);
+console.log(x >= 1);
+console.log(x < 1);
+console.log(x <= 1);
+
+// Equality
+console.log(x === 1);
+console.log(x !== 1);
