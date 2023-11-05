b8b9ae9bf684c0025facd5f9b88bde84ad1895a2
Yosendra <yosendra.evri@gmail.com>
2023-11-05 11:39:01 +0800

    Operators - 6. Logical Operators with Boolean

---
+++ 1. Fundamentals/2. Operators/6-Logical-Operator-with-Boolean/index.js
@@ -0,0 +1,16 @@
+// Logical AND (&&)
+// Returns true, if both operands are true
+console.log(true && true);      // true
+console.log(false && true);     // false
+console.log(false && false);    // false
+
+// Logical OR (||)
+// Returns true, one of the operands is true
+console.log(true || true);      // true
+console.log(false || true);     // true
+console.log(false || false);    // false
+
+// Logical NOT (!)
+// It negates the boolean value for one operand
+console.log(!true);             // false
+console.log(!false);            // true
