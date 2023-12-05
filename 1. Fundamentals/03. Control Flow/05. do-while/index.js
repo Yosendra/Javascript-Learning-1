9b6c7451ebe27223ec53603fc8c08ea203eb745c
Yosendra <yosendra.evri@gmail.com>
2023-12-05 23:47:40 +0800

    Control Flow - 05. do-while

---
+++ 1. Fundamentals/03. Control Flow/05. do-while/index.js
@@ -0,0 +1,9 @@
+
+// do-while
+//
+// 'do-while' will execute its statement block at least once even if the condition is not satisfied
+let i = 0;
+do {
+    console.log("Hello World");
+    i++;
+} while (false);
