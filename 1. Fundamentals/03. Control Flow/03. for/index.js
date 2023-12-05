ceaece00e5135fcfac7b62219f4af4bedb43c971
Yosendra <yosendra.evri@gmail.com>
2023-12-05 23:33:34 +0800

    Control Flow - 03. for

---
+++ 1. Fundamentals/03. Control Flow/03. for/index.js
@@ -0,0 +1,9 @@
+
+//  for(initial_expression; conditional_expression; increment_expression) {
+//      statement..  
+//  }
+
+// Print 'Hello World!' 5 times
+for (let i = 0; i < 5; i++) {
+    console.log('Hello World!');
+}
