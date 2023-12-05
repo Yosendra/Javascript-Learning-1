251bf5b59a2c52090b224f3355a0a6892bc6bd46
Yosendra <yosendra.evri@gmail.com>
2023-12-05 23:40:25 +0800

    Control Flow - 04. while

---
+++ 1. Fundamentals/03. Control Flow/04. while/index.js
@@ -0,0 +1,18 @@
+
+// 'for-loop' version
+for (let i = 0; i < 5; i++) {
+    console.log('Hello World!');
+}
+
+// 'while-loop' version
+//
+// control variable definition
+// while(condition){
+//   statement...
+//   increment/decrement of control variable..   
+// }
+let i = 0;
+while (i < 5) {
+    console.log('Hello World!');
+    i++;
+}
