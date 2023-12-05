d1ba8d77ba3d02452454cd8a5d2585904ba8e4f0
Yosendra <yosendra.evri@gmail.com>
2023-12-06 00:14:11 +0800

    Control Flow - 08. for-of

---
+++ 1. Fundamentals/03. Control Flow/08. for-of/index.js
@@ -0,0 +1,7 @@
+
+// 'for-of' loop, the recommended way to iterate array
+const colors = ['red', 'green', 'blue'];
+for (let color of colors) {
+    // it get the value, not the key
+    console.log(color)
+}
