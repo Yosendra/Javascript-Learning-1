03bbd8e48a035513cffa033b5222bec5e18766ee
Yosendra <yosendra.evri@gmail.com>
2023-12-06 17:22:29 +0800

    Control Flow - 17. Sum of Multiples 3 and 5

---
+++ 1. Fundamentals/03. Control Flow/17. Exercise - Sum of Multiples 3 and 5/index.js
@@ -0,0 +1,13 @@
+
+// Sum of Multiples 3 and 5
+function sum(limit) {
+    let total = 0;
+
+    for (let i = 0; i <= limit; i++)
+        if (i % 3 === 0 || i % 5 === 0)
+            total += i;
+
+    return total;
+}
+
+console.log(sum(10));
