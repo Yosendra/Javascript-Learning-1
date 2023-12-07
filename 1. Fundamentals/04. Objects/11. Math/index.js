f152d402309834ce60338c4fe588b32258c578af
Yosendra <yosendra.evri@gmail.com>
2023-12-07 11:34:22 +0800

    Object - 11. Math

---
+++ 1. Fundamentals/04. Objects/11. Math/index.js
@@ -0,0 +1,6 @@
+
+// 'Math' is a built-in object provided by Javascript. It used to be used in math related stuff.
+console.log(Math.random());
+console.log(Math.PI);
+console.log(Math.max(1, 2, 3, 4, 5));
+
