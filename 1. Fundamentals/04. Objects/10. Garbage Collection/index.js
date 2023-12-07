9625a11eeb4102e9dbc30f4132effd27813cfba4
Yosendra <yosendra.evri@gmail.com>
2023-12-07 11:28:48 +0800

    Object - 10. Garbage Collection

---
+++ 1. Fundamentals/04. Objects/10. Garbage Collection/index.js
@@ -0,0 +1,8 @@
+
+// In low level language like C and C++, we need manually allocate a memory and also deallocate the memory
+// In javascript there is no need to do that because we have 'Garbage Collector'
+
+let circle = {}         // this allocate a memory
+console.log(circle);
+
+// end of execution, all memory allocated will be released automatically by 'Garbage Collector'
