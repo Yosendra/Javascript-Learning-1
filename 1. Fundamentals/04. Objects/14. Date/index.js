6d47149714a23605076ac4c0b7ce7f9cc3442979
Yosendra <yosendra.evri@gmail.com>
2023-12-07 12:02:24 +0800

    Object - 14. Date

---
+++ 1. Fundamentals/04. Objects/14. Date/index.js
@@ -0,0 +1,4 @@
+
+const now = new Date();
+const date1 = new Date("May 11 2018 09:00");    // look at documentation to see more string date format supported in Date constructor
+const date2 = new Date(2018, 4, 11, 9);         // January is at 0, so May will be in 4
