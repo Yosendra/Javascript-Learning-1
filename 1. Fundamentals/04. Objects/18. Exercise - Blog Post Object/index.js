6f0d87582864c6600ee80ad5b27ee521b4eae8e2
Yosendra <yosendra.evri@gmail.com>
2023-12-07 12:40:22 +0800

    Object - 18. Exercise: Blog Post Object

---
+++ 1. Fundamentals/04. Objects/18. Exercise - Blog Post Object/index.js
@@ -0,0 +1,26 @@
+
+// title
+// body
+// author
+// view
+// comments
+//  (author, body)
+// isLive
+
+const blogPost = {
+    title: "A",
+    body: "B",
+    author: "C",
+    views: 10,
+    comments: [
+        {
+            author: "A1",
+            body: "Lorem Ipsum"
+        },
+        {
+            author: "A2",
+            body: "Lorem Ipsum"
+        }
+    ],
+    isLive: false
+};
