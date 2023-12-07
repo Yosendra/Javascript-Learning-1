a609308c4cb456bb7afaf3545fe47831d69c6515
Yosendra <yosendra.evri@gmail.com>
2023-12-07 11:44:58 +0800

    Object - 12. String

---
+++ 1. Fundamentals/04. Objects/12. String/index.js
@@ -0,0 +1,16 @@
+
+// string primitive
+const message = 'hi';
+console.log(typeof message)
+
+// String object
+const another = new String('hi');
+console.log(typeof another)
+
+// Look more at Mozilla Documentation
+console.log(message.length)
+console.log(message.includes('hi'))
+console.log(message.includes('not'))
+console.log(message[0])
+console.log(message[1])
+console.log(message.toUpperCase())
