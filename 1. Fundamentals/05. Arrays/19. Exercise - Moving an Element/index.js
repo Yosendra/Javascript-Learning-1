0a80247680a4b72ad4c5e76615e51fccdd27b05b
Yosendra <yosendra.evri@gmail.com>
2023-12-09 15:59:33 +0800

    Array - 19. Exercise: Moving an Element

---
+++ 1. Fundamentals/05. Arrays/19. Exercise - Moving an Element/index.js
@@ -0,0 +1,21 @@
+
+const numbers = [1, 2, 3, 4]
+const output = move(numbers, 0, 2)
+
+console.log(output);
+
+function move(array, index, offset) {
+    const position = index + offset
+    if (position >= array.length || position < 0) {
+        console.log("Invalid offset");
+        return
+    }
+
+    const output = [...array]
+    const element = output.splice(index, 1)[0]
+    
+    // console.log(output);    // notice the 'output' has been modified, one element has been removed
+
+    output.splice(position, 0, element)
+    return output
+}
